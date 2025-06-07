31731e421ab13f741d623f6973fe4a7b62cdb752
4EyeCodes <EyeCodes@users.noreply.github.com>
2025-06-08 00:45:43 +0800

    updated views
    added folders such as pages, layouts, and auth. Making sure we will be only using extends to include contents

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <header>
+    
+  </header>
+  <main>
+    <aside>
+{{-- sidebar
+      here where all notes/task show up--}}
+      {{-- TODO:
+            create template--}}
+    </aside>
+    <div>
+      {{--add via contents/yield --}}
+      @yield('content')
+    </div>
+  </main>
+  <footer></footer>
+</body>
+</html>
